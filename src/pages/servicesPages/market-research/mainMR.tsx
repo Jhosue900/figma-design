ae025194451a6cafcbd46bdb9a5aa78540b3db66
jhon-henao13 <jhonestiwarhenao@gmail.com>
2026-05-27 15:27:51 -0500

    fix(mkt-research): Hero reestructurados & Add section brands

--- src/pages/servicesPages/market-research/mainMR.tsx
+++ src/pages/servicesPages/market-research/mainMR.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import Navbar from '../../../components/General/Navbar';
 import Footer from '../../../components/General/Footer';
 import HeroMR from './Hero';
+import TwoSectionBrands from './TwoSectionBrands';
 import WhyMR from './whyMR';
 import ExperienciaMR from './experienciaMR';
 import ContactoMR from './contactMR';
@@ -47,6 +48,7 @@ export default function MarketResearch() {
         <div className="min-h-screen bg-transparent overflow-x-hidden">
             <Navbar />
             <HeroMR />
+            <ScrollReveal><TwoSectionBrands /></ScrollReveal>
             <ScrollReveal><WhyMR /></ScrollReveal>
             <ScrollReveal><ExperienciaMR /></ScrollReveal>
             <ScrollReveal><TestimonialMR /></ScrollReveal>
